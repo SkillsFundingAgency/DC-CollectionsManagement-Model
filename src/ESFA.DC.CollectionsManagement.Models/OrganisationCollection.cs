8dc1363f64c660da1d0181e55d127f4c5bf67dd4
Salman Baig <salman.sfa@outlook.com>
2018-07-12 15:22:37 +0100

    initial model objects

---
+++ src/ESFA.DC.CollectionsManagement.Models/OrganisationCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFA.DC.CollectionsManagement.Models
+{
+    public class OrganisationCollection
+    {
+        public long Ukrpn { get; set; }
+        public Collection CollectionType { get; set; }
+    }
+}
---
+++ src/ESFA.DC.CollectionsManagement.Models/ReturnCalendar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFA.DC.CollectionsManagement.Models
+{
+    public class ReturnCalendar
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string PeriodName { get; set; }
+        public int CalendarMonth { get; set; }
+        public int CalendarYear { get; set; }
+        public Collection Collection { get; set; }
+    }
+}
