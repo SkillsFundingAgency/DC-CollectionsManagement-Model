ab0b1f1a89d3622b82d69ac13a3a14945e2b4484
Salman Baig <salman.sfa@outlook.com>
2018-07-13 16:23:49 +0100

    added database project

---
+++ src/ESFA.DC.CollectionsManagement.Database/dbo/Tables/ReturnPeriod.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ReturnPeriod] (
+    [ReturnPeriodId]   INT          NOT NULL,
+    [StartDateTimeUTC] DATETIME     NOT NULL,
+    [EndDateTimeUTC]   DATETIME     NOT NULL,
+    [PeriodName]       VARCHAR (50) NOT NULL,
+    [CollectionId]     INT          NOT NULL,
+    [CalendarMonth]    INT          NOT NULL,
+    [CalendarYear]     INT          NOT NULL,
+    CONSTRAINT [PK_ReturnPeriod] PRIMARY KEY CLUSTERED ([ReturnPeriodId] ASC),
+    CONSTRAINT [FK_ReturnPeriod_Collection] FOREIGN KEY ([CollectionId]) REFERENCES [dbo].[Collection] ([CollectionId])
+);
+
